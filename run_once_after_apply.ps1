3a68bf0fbbf3c01c0b85dd7d0c7afe7a49ffa037
Kinetic-Energyy <nikobellicmanisk@gmail.com>
2024-12-27 23:17:02 +0530

    added quick

--- run_once_after_apply.ps1
+++ run_once_after_apply.ps1
@@ -1,4 +1,2 @@
-
-
 # run_once_after_apply.ps1
-Write-Host "Done applying all configurations with chezmoi!" -ForegroundColor Green
+Write-Host "Done applying all configurations with chezmoi! chaneged here check _____---____" -ForegroundColor Green
