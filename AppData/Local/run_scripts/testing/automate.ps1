4597319cbba4619e48bdb892bd8fea756294f0b1
Kinetic-Energyy <nikobellicmanisk@gmail.com>
2024-12-28 12:13:06 +0530

    added quick

--- AppData/Local/run_scripts/testing/automate.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Starting automation"
-Set-Location -Path "$HOME\.local\share\chezmoi"
-git add .
-$userinp = Read-Host -Prompt "Enter commit message"
-git commit -m "$userinp"
-git push -u origin master
-
--- AppData/Local/run_scripts/testing/refresh.ps1
+++
@@ -1,7 +0,0 @@
-$modifiedFiles = chezmoi status | Where-Object { $_ -match 'MM' }
-foreach ($file in $modifiedFiles) {
-    $filePath = $file.Trim() -replace '^MM\s*', ''
-    $absolutePath = Join-Path $env:USERPROFILE $filePath
-    Write-Host $absolutePath
-    chezmoi add $absolutePath
-}
