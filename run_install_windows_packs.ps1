d2aba7ebd7cf64990119a9b7bec764ecd276c265
Kinetic-Energyy <nikobellicmanisk@gmail.com>
2024-12-28 14:00:04 +0530

    removed autohotkey dummy scripts

--- run_install_windows_packs.ps1
+++ run_install_windows_packs.ps1
@@ -26,33 +26,8 @@ function osd-layout {
 
     Write-Host "Files copied successfully to the target locations."
 }
-osd-layout
+# osd-layout
 
-function copy-autohotkey-scripts {
-    $sourcePath = "$HOME\.local\share\chezmoi\appdata\local\autohotkey"
-    $startupFolder = Join-Path $env:APPDATA "Microsoft\Windows\Start Menu\Programs\Startup"
-    $ahkPath = "C:\Program Files\AutoHotkey\AutoHotkey.exe"  # Update the path if AutoHotkey is installed elsewhere
-
-    # Ensure the source directory exists
-    if (Test-Path $sourcePath) {
-        Write-Host "Running AutoHotkey scripts from $sourcePath..."
-        
-        # Get all .ahk files in the source directory and run them
-        $scripts = Get-ChildItem -Path $sourcePath -Filter *.ahk
-        foreach ($script in $scripts) {
-            Write-Host "Running script: $($script.FullName)"
-            # Run each script using AutoHotkey
-            Start-Process $ahkPath -ArgumentList $script.FullName
-        }
-
-        Write-Host "Copying AutoHotkey scripts from $sourcePath to $startupFolder..."
-        Copy-Item -Path "$sourcePath\*" -Destination $startupFolder -Recurse -Force
-        Write-Host "AutoHotkey scripts copied successfully to the Startup folder."
-    }
-    else {
-        Write-Host "Source directory $sourcePath does not exist."
-    }
-}
 
 function startup () {
     $startupFolder = Join-Path $env:APPDATA "Microsoft\Windows\Start Menu\Programs\Startup"
@@ -67,18 +42,5 @@ function startup () {
     Write-Host "Startup function completed."
 }
 startup
-# function prompt-user {
-#     $response = Read-Host "Do you want to proceed terminal and powershell 7? (yes/no)"
-#     if ($response -eq "yes") {
-#         Write-Host "User chose to proceed with the task."
-#         choco install powershell-core microsoft-windows-terminal
-#         copy-autohotkey-scripts
-#     }
-#     else {
-#         Write-Host "User chose not to proceed with the task."
-#     }
-# }
-# prompt-user
 
-
-Write-Host "run_install_windows_packs.ps1 ran hhere old one" -ForegroundColor Green
+Write-Host "run_install_windows_packs.ps1 ran" -ForegroundColor Green
