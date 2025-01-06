50804c6b504f2b30f8dc96a34cbc86aff5a9c040
Kinetic-Energyy <nikobellicmanisk@gmail.com>
2025-01-06 20:59:24 +0530

    added lazyily .files

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -700,11 +700,12 @@ if (Get-Command zoxide -ErrorAction SilentlyContinue) {
     Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
 }
 else {
-    Write-Host "zoxide command not found. Attempting to install via winget..."
+    #Write-Host "zoxide command not found. Attempting to install via winget..."
     try {
-        winget install -e --id ajeetdsouza.zoxide
-        Write-Host "zoxide installed successfully. Initializing..."
-        Invoke-Expression (& { (zoxide init powershell | Out-String) })
+        #winget install -e --id ajeetdsouza.zoxide
+        #Write-Host "zoxide installed successfully. Initializing..."
+        Write-Host "zoxide not installed"
+        #Invoke-Expression (& { (zoxide init powershell | Out-String) })
     }
     catch {
         Write-Error "Failed to install zoxide. Error: $_"
