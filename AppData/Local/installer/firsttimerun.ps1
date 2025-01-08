794363b1e8735b7bbc4f0b0518c0d67fa1d85acc
Kinetic-Energyy <nikobellicmanisk@gmail.com>
2025-01-08 19:58:00 +0530

    added lazyily .files

--- AppData/Local/installer/firsttimerun.ps1
+++ AppData/Local/installer/firsttimerun.ps1
@@ -17,17 +17,30 @@ function Install-Winget {
 	}
 }
 
+
+function Check-And-AddBucket {
+	param (
+		[string]$bucketName,
+		[string]$bucketUrl
+	)
+
+	# Get the list of existing buckets
+	$existingBuckets = scoop bucket list
+
+	# Check if the bucket is already in the list
+	if ($existingBuckets -notcontains $bucketName) {
+		Write-Host "Adding Scoop bucket: $bucketName"
+		scoop bucket add $bucketName $bucketUrl
+	}
+	else {
+		Write-Host "Scoop bucket '$bucketName' already exists."
+	}
+}
 # Function to install Scoop
 function Install-Scoop {
 	if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
 		Write-Host "Installing Scoop..."
 		if (-not (Is-Admin)) {
-			# Write-Host "Installation under the administrator console has been disabled by default for security considerations."
-			# Write-Host "If you know what you are doing and want to install Scoop as administrator, please download the installer and manually execute it with the -RunAsAdmin parameter."
-			# Write-Host "Example:"
-			# Write-Host "irm get.scoop.sh -outfile 'install.ps1'"
-			# Write-Host ".\install.ps1 -RunAsAdmin"
-			# Write-Host "Or use the one-liner command:"
 			Write-Host "Installing Scoop"
 			Write-Host "iex ""& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"""
 		}
@@ -36,48 +49,81 @@ function Install-Scoop {
 			Invoke-Expression "& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"
 		}
 	}
- else {
+	else {
 		Write-Host "Scoop is already installed."
-		scoop bucket add extras
-		scoop bucket add java
-		scoop bucket add versions
-		scoop bucket add nerd-fonts
+		scoop update
+		scoop bucket update *
 
-
-		# Define Scoop buckets (excluding main and extras)
-		# $scoopBuckets = @(
-		# 	@{ Name = "versions"; URL = "https://github.com/ScoopInstaller/Versions.git" },
-		# 	@{ Name = "nerd-fonts"; URL = "https://github.com/matthewjberger/scoop-nerd-fonts.git" },
-		# 	@{ Name = "shemnei"; URL = "https://github.com/Shemnei/scoop-bucket.git" },
-		# 	@{ Name = "volllly"; URL = "https://github.com/volllly/scoop-bucket.git" }
-		# )
-		#
-		# # Check and add buckets
-		# foreach ($bucket in $scoopBuckets) {
-		# 	$bucketInfo = scoop bucket list | Where-Object { $_ -match $bucket.Name }
-		#
-		# 	if (-not $bucketInfo) {
-		# 		# If bucket is not listed, add it
-		# 		Write-Host "Adding Scoop bucket: $($bucket.Name)"
-		# 		scoop bucket add $($bucket.Name) $($bucket.URL)
-		# 	}
-		# 	else {
-		# 		# Extract manifest count
-		# 		$manifestCount = ($bucketInfo -split '\s+')[-1]
-		# 		if ([int]$manifestCount -eq 0) {
-		# 			# If manifest count is 0, re-add the bucket
-		# 			Write-Host "The '$($bucket.Name)' bucket has 0 manifests. Re-adding..."
-		# 			scoop bucket rm $($bucket.Name)
-		# 			scoop bucket add $($bucket.Name) $($bucket.URL)
-		# 		}
-		# 		else {
-		# 			Write-Host "The '$($bucket.Name)' bucket already exists with $manifestCount manifests."
-		# 		}
-		# 	}
-		# }
-
+		# Check and add buckets
+		Check-And-AddBucket -bucketName "extras" -bucketUrl ""
+		Check-And-AddBucket -bucketName "java" -bucketUrl ""
+		Check-And-AddBucket -bucketName "versions" -bucketUrl ""
+		Check-And-AddBucket -bucketName "nerd-fonts" -bucketUrl ""
+		Check-And-AddBucket -bucketName "volllly" -bucketUrl "https://github.com/volllly/scoop-bucket.git"
+		Check-And-AddBucket -bucketName "shemnei" -bucketUrl "https://github.com/Shemnei/scoop-bucket.git"
 	}
 }
+# Function to install Scoop
+# function Install-Scoop {
+# 	if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+# 		Write-Host "Installing Scoop..."
+# 		if (-not (Is-Admin)) {
+# 			# Write-Host "Installation under the administrator console has been disabled by default for security considerations."
+# 			# Write-Host "If you know what you are doing and want to install Scoop as administrator, please download the installer and manually execute it with the -RunAsAdmin parameter."
+# 			# Write-Host "Example:"
+# 			# Write-Host "irm get.scoop.sh -outfile 'install.ps1'"
+# 			# Write-Host ".\install.ps1 -RunAsAdmin"
+# 			# Write-Host "Or use the one-liner command:"
+# 			Write-Host "Installing Scoop"
+# 			Write-Host "iex ""& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"""
+# 		}
+# 		else {
+# 			Write-Host "Running Scoop installation with elevated permissions..."
+# 			Invoke-Expression "& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"
+# 		}
+# 	}
+#  else {
+# 		Write-Host "Scoop is already installed."
+# 		scoop bucket add extras
+# 		scoop bucket add java
+# 		scoop bucket add versions
+# 		scoop bucket add nerd-fonts
+#
+#
+# 		# Define Scoop buckets (excluding main and extras)
+# 		# $scoopBuckets = @(
+# 		# 	@{ Name = "versions"; URL = "https://github.com/ScoopInstaller/Versions.git" },
+# 		# 	@{ Name = "nerd-fonts"; URL = "https://github.com/matthewjberger/scoop-nerd-fonts.git" },
+# 		# 	@{ Name = "shemnei"; URL = "https://github.com/Shemnei/scoop-bucket.git" },
+# 		# 	@{ Name = "volllly"; URL = "https://github.com/volllly/scoop-bucket.git" }
+# 		# )
+# 		#
+# 		# # Check and add buckets
+# 		# foreach ($bucket in $scoopBuckets) {
+# 		# 	$bucketInfo = scoop bucket list | Where-Object { $_ -match $bucket.Name }
+# 		#
+# 		# 	if (-not $bucketInfo) {
+# 		# 		# If bucket is not listed, add it
+# 		# 		Write-Host "Adding Scoop bucket: $($bucket.Name)"
+# 		# 		scoop bucket add $($bucket.Name) $($bucket.URL)
+# 		# 	}
+# 		# 	else {
+# 		# 		# Extract manifest count
+# 		# 		$manifestCount = ($bucketInfo -split '\s+')[-1]
+# 		# 		if ([int]$manifestCount -eq 0) {
+# 		# 			# If manifest count is 0, re-add the bucket
+# 		# 			Write-Host "The '$($bucket.Name)' bucket has 0 manifests. Re-adding..."
+# 		# 			scoop bucket rm $($bucket.Name)
+# 		# 			scoop bucket add $($bucket.Name) $($bucket.URL)
+# 		# 		}
+# 		# 		else {
+# 		# 			Write-Host "The '$($bucket.Name)' bucket already exists with $manifestCount manifests."
+# 		# 		}
+# 		# 	}
+# 		# }
+#
+# 	}
+# }
 
 # Function to install Chocolatey
 function Install-Chocolatey {
