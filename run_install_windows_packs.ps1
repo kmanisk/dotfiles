85bfec9dd7311a0b069f5b43547e7542a102ede4
kmanisk <mathewmanisk699@gmail.com>
2025-01-17 13:40:10 -0800

    added lazyily .files

--- run_install_windows_packs.ps1
+++ run_install_windows_packs.ps1
@@ -740,5 +740,3 @@ function Pin-WingetPackage {
 Pin-ChocoPackage -packageName "zoxide"
 Pin-ChocoPackage -packageName "autohotkey"
 Pin-WingetPackage -packageId "AutoHotkey.AutoHotkey"
-
-scoop install ntop
