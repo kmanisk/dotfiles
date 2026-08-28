e99b5f3a523a37614583fb6ee3ae939d1d78732b
Kumar Manisk <mathewmanisk699@gmail.com>
2026-08-28 15:57:25 +0530

    fixed tabs sized

--- AppData/Local/nvim/lua/chadrc.lua
+++ AppData/Local/nvim/lua/chadrc.lua
@@ -2,7 +2,7 @@
 local M = {}
 -- UI Configuration
 M.ui = {
-    theme = "embark", -- Set the theme for Neovim
+    theme = "dark_horizon", -- Set the theme for Neovim
     transparency = true, -- Enable transparency
     -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
@@ -34,12 +34,12 @@ M.ui = {
 }
 
 M.base46 = {
-    theme = "embark", -- Change to your preferred base46 theme (e.g., 'tokyonight', 'gruvbox', etc.)
+    theme = "dark_horizon", -- Change to your preferred base46 theme (e.g., 'tokyonight', 'gruvbox', etc.)
     hl_add = {},
     hl_override = {},
     changed_themes = {},
     transparency = false,
-    theme_toggle = { "embark", "one_light" },
+    theme_toggle = { "dark_horizon", "one_light" },
 }
 
 M.nvdash = {
