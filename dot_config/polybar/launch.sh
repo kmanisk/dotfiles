fbbd4a7b5d1a583146286145d49122b9e57029ff
Kumar Manisk <mathewmanisk699@gmail.com>
2026-09-09 23:47:09 +0530

    feat(i3): bind Alt+9 to toggle bottom bar (polybar) visibility

--- dot_config/polybar/launch.sh
+++ dot_config/polybar/launch.sh
@@ -4,13 +4,13 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
-# for multimonitor
-if type "xrandr"; then
+# Launch polybar detached
+if type "xrandr" >/dev/null 2>&1; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example & disown
+    MONITOR=$m polybar --reload example </dev/null >/dev/null 2>&1 &
   done
 else
-  polybar --reload example & disown
+  polybar --reload example </dev/null >/dev/null 2>&1 &
 fi
