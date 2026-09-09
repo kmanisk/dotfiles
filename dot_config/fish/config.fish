5583db5150d3c91374f59880e9d75f06268ba881
Kumar Manisk <mathewmanisk699@gmail.com>
2026-09-09 23:27:07 +0530

    feat(linux): add i3-resurrect session manager, polybar/gtk scaling, and sync dotfiles

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,11 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+fish_add_path -m ~/.local/bin
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
